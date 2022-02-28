732008ed095cd48392a87ade849f96845756b80f
d <roseannee.kim@gmail.com>
2022-02-28 23:30:18 +0100

    Добавьте файлы проекта.

---
+++ lab1/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel = new System.Windows.Forms.Panel();
+            this.coordinates = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.mouseButtons = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // panel
+            // 
+            this.panel.Location = new System.Drawing.Point(9, 10);
+            this.panel.Margin = new System.Windows.Forms.Padding(2);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(472, 304);
+            this.panel.TabIndex = 0;
+            this.panel.Paint += new System.Windows.Forms.PaintEventHandler(this.panel_Paint);
+            this.panel.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel_MouseClick);
+            this.panel.MouseEnter += new System.EventHandler(this.panel_MouseEnter);
+            this.panel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel_MouseMove);
+            // 
+            // coordinates
+            // 
+            this.coordinates.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.coordinates.Location = new System.Drawing.Point(9, 335);
+            this.coordinates.Margin = new System.Windows.Forms.Padding(2);
+            this.coordinates.Name = "coordinates";
+            this.coordinates.Size = new System.Drawing.Size(228, 23);
+            this.coordinates.TabIndex = 0;
+            this.coordinates.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(83, 316);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "coordinates";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(324, 316);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "mouse buttons";
+            // 
+            // mouseButtons
+            // 
+            this.mouseButtons.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.mouseButtons.Location = new System.Drawing.Point(255, 335);
+            this.mouseButtons.Margin = new System.Windows.Forms.Padding(2);
+            this.mouseButtons.Name = "mouseButtons";
+            this.mouseButtons.Size = new System.Drawing.Size(228, 23);
+            this.mouseButtons.TabIndex = 2;
+            this.mouseButtons.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(490, 366);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.mouseButtons);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.coordinates);
+            this.Controls.Add(this.panel);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Lab 1 - Mouse Events";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.TextBox coordinates;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox mouseButtons;
+    }
+}
+
---
+++ lab1/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace lab1
+{
+    public partial class Form1 : Form
+    {
+        static Dictionary<int, int> circles = new Dictionary<int, int>();
+        static Dictionary<int, int> rectangles = new Dictionary<int, int>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel_MouseClick(object sender, MouseEventArgs e)
+        {
+            try
+            {
+                if (e.Button == MouseButtons.Left)
+                {
+                    circles.Add(e.X, e.Y);
+
+                    mouseButtons.Text = MouseButtons.Left.ToString();
+
+                    panel.Refresh();
+                }
+                else if (e.Button == MouseButtons.Right)
+                {
+                    rectangles.Add(e.X, e.Y);
+
+                    mouseButtons.Text = MouseButtons.Right.ToString();
+
+                    panel.Refresh();
+                }
+                else
+                {
+                    mouseButtons.Text = MouseButtons.Middle.ToString();
+                }
+            }
+            catch { }
+        }
+
+        private void panel_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = panel.CreateGraphics();
+
+            foreach (var circle in circles)
+            {
+                g.FillEllipse(Brushes.Blue, circle.Key - 25, circle.Value - 25, 50, 50);
+            }
+
+            foreach (var rectangle in rectangles)
+            {
+                g.FillRectangle(Brushes.Red, rectangle.Key - 25, rectangle.Value - 25, 50, 50);
+            }
+        }
+
+        private void panel_MouseMove(object sender, MouseEventArgs e)
+        {
+            coordinates.Text = $"X: {e.X}; Y: {e.Y}";
+        }
+
+        private void panel_MouseEnter(object sender, EventArgs e)
+        {
+            panel.Cursor = Cursors.Cross;
+        }
+    }
+}
